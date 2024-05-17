30ff8fc502e5f0b046a75c5febd7b0e91466c429
morgan_p <morgan.prieur@tetradis.com>
2024-05-17 16:15:05 +0200

    WIP tests. Coverage 0% to correct.

--- tests/tests_oc_lettings.py
+++ tests/tests_oc_lettings.py
@@ -1,9 +1,6 @@
-from django.test import TestCase
+# from django.test import TestCase
 
-import pytest
 from django.urls import reverse, resolve
-from lettings.models import Letting
-# from lettings.models import Address, Letting
 from django.test import Client
 from pytest_django.asserts import assertTemplateUsed
 
@@ -30,20 +27,3 @@ def test_admin_url():
     path = reverse("admin:index")
     assert path == "/admin/"
     assert resolve(path).view_name == "admin:index"
-
-
-class LettingsUrlsTestCase(TestCase):
-    @pytest.fixture(autouse=True)
-    def prepare_fixture(self, one_address):
-        self.one_address = one_address
-
-    def setUp(self):
-        """setup an instance of letting and address"""
-        self.address = self.one_address
-        self.letting = Letting.objects.create(title="My letting", address=self.address)
-
-    def test_letting_index_url(self):
-        """test index url"""
-        path = reverse("lettings:index")
-        assert path == "/lettings/"
-        assert resolve(path).view_name == "lettings:index"
