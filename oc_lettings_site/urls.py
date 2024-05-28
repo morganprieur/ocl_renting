6fe24c1f8bac4b8ac6a258fe7196fb4a239a28cc
Morgan Prieur <morgan.prieur@tetradis.com>
2024-05-28 15:58:04 +0200

    Fixed staticfiles + error 500.

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -6,18 +6,16 @@ from django.views.static import serve
 
 from . import views
 
-handler404 = "oc_lettings_site.views.page_not_found"
-# handler500 = "oc_lettings_site.views.server_error"
-handler500 = views.server_error
+handler404 = 'oc_lettings_site.views.page_not_found'
+handler500 = 'oc_lettings_site.views.server_error'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('lettings/', include('lettings.urls', namespace='lettings')),
     path("profiles/", include('profiles.urls', namespace='profiles')),
-    path("hello_world_error/", views.hello_world_error),
+    path("hello_world/", views.hello_world),
 
-    re_path(r'ŝtatic/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-    # re_path(r'ŝtatic/(?p<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('', views.index, name='index'),
 ]
--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -10,70 +10,10 @@ def index(request):
 def page_not_found(request, exception):
     return render(request, '404.html', status=404)
 
-# def page_not_found(request, exception):
-#     response = render(request, "404.html", {})
-#     response.status_code = 404
-#     return response
-
 
 def server_error(request):
     return render(request, '500.html', status=500)
 
-# def server_error(request, exception=None):
-#     response = render(request, "500.html", {
-#         'exception': exception
-#     })
-#     response.status_code = 500
-#     return response
 
-
-def hello_world_error(request):
+def hello_world(request):
     raise Exception('This is a test error')
-
-
-# # Aenean leo magna, vestibulum et tincidunt fermentum, consectetur quis velit. Sed non placerat
-# massa. Integer est nunc, pulvinar a
-# # tempor et, bibendum id arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
-# posuere cubilia curae; Cras eget scelerisque
-# def lettings_index(request):
-#     lettings_list = Letting.objects.all()
-#     context = {'lettings_list': lettings_list}
-#     return render(request, 'lettings_index.html', context)
-
-
-# #Cras ultricies dignissim purus, vitae hendrerit ex varius non. In accumsan porta nisl id
-# eleifend. Praesent dignissim, odio eu consequat pretium, purus urna vulputate arcu, vitae
-# efficitur
-# #  lacus justo nec purus. Aenean finibus faucibus lectus at porta. Maecenas auctor, est ut
-# luctus congue, dui enim mattis enim, ac condimentum velit libero in magna. Suspendisse potenti.
-# In tempus a nisi sed laoreet.
-# # Suspendisse porta dui eget sem accumsan interdum. Ut quis urna pellentesque justo mattis
-# ullamcorper ac non tellus. In tristique mauris eu velit fermentum, tempus pharetra est luctus.
-# Vivamus consequat aliquam libero, eget bibendum lorem. Sed non dolor risus. Mauris condimentum
-# auctor elementum. Donec quis nisi ligula. Integer vehicula tincidunt enim, ac lacinia augue
-# pulvinar sit amet.
-# def letting(request, letting_id):
-#     letting = Letting.objects.get(id=letting_id)
-#     context = {
-#         'title': letting.title,
-#         'address': letting.address,
-#     }
-#     return render(request, 'letting.html', context)
-
-# # Sed placerat quam in pulvinar commodo. Nullam laoreet consectetur ex, sed consequat libero
-# pulvinar eget. Fusc
-# # faucibus, urna quis auctor pharetra, massa dolor cursus neque, quis dictum lacus d
-# def profiles_index(request):
-#     profiles_list = Profile.objects.all()
-#     context = {'profiles_list': profiles_list}
-#     return render(request, 'profiles_index.html', context)
-
-# # Aliquam sed metus eget nisi tincidunt ornare accumsan eget lac
-# # laoreet neque quis, pellentesque dui. Nullam facilisis pharetra vulputate. Sed tincidunt,
-# dolor id facilisis fringilla, eros leo tristique lacus,
-# # it. Nam aliquam dignissim congue. Pellentesque habitant morbi tristique senectus et netus et
-# males
-# def profile(request, username):
-#     profile = Profile.objects.get(user__username=username)
-#     context = {'profile': profile}
-#     return render(request, 'profile.html', context)
